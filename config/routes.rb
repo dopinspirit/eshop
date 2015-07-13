ffd12dd1a6ec58e55f1587907ed2023ca472e74a
dopinspirit <xtwinpeaks@gmail.com>
2015-07-13 21:40:39 +0300

    generated views and controllers for devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {
+                       sessions: 'users/sessions',
+                       registrations: 'users/registrations'
+                   }
+
 
   root 'navigator#index'
 
+
   get '/category_products', to: 'navigator#products_from_category', as: 'category_products'
   get '/product_details', to: 'navigator#product_details', as: 'product_details'
+
 
   post '/create_review', to: 'navigator#product_review', as: 'product_review'
 
